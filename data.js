b89e88f416995af6d62a265f1bee7483b1ae6316
De Oka <115537848+DeOkanee@users.noreply.github.com>
2026-03-16 19:10:41 +0800

    new images2

--- data.js
+++ data.js
@@ -10,5 +10,17 @@ const imageData = {
         "BNZ_3381.jpg",
         "BNZ_3457.jpg",
         "BNZ_3546.jpg"
+    ],
+    "images2": [
+        "BNZ_2897.webp",
+        "BNZ_3034.webp",
+        "BNZ_3164.webp",
+        "BNZ_3174.webp",
+        "BNZ_3231.webp",
+        "BNZ_3294.webp",
+        "BNZ_3325.webp",
+        "BNZ_3381.webp",
+        "BNZ_3457.webp",
+        "BNZ_3546.webp"
     ]
 };
